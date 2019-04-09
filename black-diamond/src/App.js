ea58e1a3114de678c66ffa8b96c221f1449c232c
Dillon O'Kelley <dillonokelley@Dillons-MacBook-Air.local>
2019-04-09 15:41:04 -0600

    third commit black diamond fix

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.addToCart = this.addToCart.bind(this)
+    this.removeFromCart = this.removeFromCart.bind(this)
+    this.navigate = this.navigate.bind(this)
   }
   componentDidMount() {
     axios
@@ -29,16 +32,16 @@ class App extends Component {
   }
   removeFromCart(index) {
     let cartCopy = this.state.cart.slice();
-    cartCopy.splice(index, 1);
+    cartCopy.splice(this.state.cart.indexOf(index), 1);
     this.setState({
       cart: cartCopy
     });
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({showCart: true});
     } else {
-      this.state.showCart = false;
+      this.setState({showCart: false});
     }
   }
   render() {
